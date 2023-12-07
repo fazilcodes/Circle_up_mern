ce6d6a50d2c57751332da4a79df36b8e20f6b7d3
Fazil <122675114+fazilcodes@users.noreply.github.com>
2023-12-07 17:54:17 +0530

    redux state setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Home from 'Pages/home/Home';
+import Login from 'Pages/login/Login';
+import Profile from 'Pages/profile/Profile';
+
 
 const App = () => {
+
   return (
     <div className='app'>
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/profile/:userId' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
